1e678ea0d52eedad1d74d2e3336f9fdc0ae21f3e
sodatea <sodazju@gmail.com>
2015-04-27 18:24:59 +0800

    Refine gulpfile and its dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,9 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
 var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
 var sourcemaps = require('gulp-sourcemaps');
-var rename = require('gulp-rename');
 var replace = require('gulp-replace');
-var sh = require('shelljs');
 var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 var paths = {
   sass: ['./www/scss/**/*.scss']
@@ -20,26 +15,6 @@ gulp.task('sass', ['ionic-sass', 'app-sass']);
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
-});
-
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
-
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
 });
 
 gulp.task('browser-sync', function() {
@@ -76,7 +51,7 @@ gulp.task('ionic-sass', function(cb) {
     .then(function(addr) { return addr.replace('file:', '').replace(/^.*jspm_packages/, './www/jspm_packages'); })
     .then(function(jsPath) {
       var p = path.parse(jsPath);
-      var dirPath = path.join(p.dir, p.name)
+      var dirPath = path.join(p.dir, p.name);
       return dirPath;
     })
     .then(function(dirPath) {
@@ -88,8 +63,7 @@ gulp.task('ionic-sass', function(cb) {
         .pipe(replace('{{ionic-font-path}}', relativeFontPath))
         .pipe(replace('{{ionic-scss-path}}', scssPath))
         .pipe(sourcemaps.init())
-        .pipe(sass({ errLogToConsole: true }))
-        // .pipe(minifyCss({ keepSpecialComments: 0 })) // not needed in development
+        .pipe(sass({errLogToConsole: true}))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./www/bundles/'))
         .on('end', cb);
