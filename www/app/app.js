9ddac4c7fd8fd7ffaaacdd39ca81af36bc817335
sodatea <sodazju@gmail.com>
2015-05-11 02:16:32 +0800

    Implement prototype of hot topics page

--- www/app/app.js
+++ www/app/app.js
@@ -4,10 +4,15 @@ import '../bundles/ionic.app.css!'; // 自定义编译后的 ionic css
 import 'driftyco/ng-cordova';
 
 import securityModule from '../common/security/security';
+import apiModule from '../common/services/api';
 
 import appRouter from './app.router';     // 路由
 import initNativeEnv from './app.native'; // 移动平台相关
 
-export default angular.module('cc98', ['ionic', securityModule.name, 'ngCordova'])
-  .config(appRouter)
+export default angular.module('cc98', [
+  'ionic',
+  'ngCordova',
+  securityModule.name,
+  apiModule.name
+]).config(appRouter)
   .run(initNativeEnv);
--- www/app/tabs/hot/hot.controller.js
+++ www/app/tabs/hot/hot.controller.js
@@ -1,10 +1,14 @@
 class HotController {
-  constructor($scope) {
+  constructor($scope, APIRequest) {
     $scope.ctrl = this;
+
     this.$scope = $scope;
+    this.APIRequest = APIRequest;
+
+    this.APIRequest.get('topic/hot').then((data) => this.topics = data);
   }
 }
 
-HotController.$inject = ['$scope'];
+HotController.$inject = ['$scope', 'APIRequest'];
 
 export default HotController;
