b2733e58d571de1683f7e828182aea1e203f3e95
sodatea <sodazju@gmail.com>
2015-06-09 23:52:02 +0800

    Beautify code

--- www/common/directives/cc98BackButton.js
+++ www/common/directives/cc98BackButton.js
@@ -1,7 +1,8 @@
 // 因为默认的 ion-nav-back-button 在目前的路由配置下没办法显示，所以写了这个
 import template from './cc98BackButton.html!text';
 
-export default angular.module('directives.cc98BackButton', ['ionic'])
+export default angular
+  .module('directives.cc98BackButton', ['ionic'])
   .directive('cc98BackButton', function($ionicNavBarDelegate) {
     return {
       restrict: 'E',
